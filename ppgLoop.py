12b67901f0b52aeab3b5b818f0137f57e852460d
ilw <ian_l_williams@hotmail.com>
2020-05-19 12:01:09 +0100

    Script & data to help testing. ppgLoop will mimic the output of the raspberrry pi - continuously dumping data to a csv file in batches of 17 samples every 68ms. The source for the data and output file can be specified using --ipfile and --opfile

---
+++ ppgLoop.py
@@ -0,0 +1,55 @@
+import csv
+import sys
+import argparse
+import time
+#from itertools import islice
+
+
+
+###########################
+#Read in Arguments & set default parameters
+###########################
+parser = argparse.ArgumentParser(description='Simulate realtime PPG data')
+parser.add_argument("--ipFile",default='19-05-2020_09-40-05_clean_with_2_breath_holds.csv',help="--ipFile='path_to_file'")
+
+parser.add_argument("--opFile",default='ppg.csv',help="--opFile='path_to_file'")
+
+args = parser.parse_args()
+
+
+SMP_FREQ = 1000.0
+AVERAGING = 4.0
+SMP_BATCH = 17.0
+
+freq = SMP_FREQ/AVERAGING
+period = 1/freq
+batch_period = SMP_BATCH * period
+
+
+rowCount =0
+loopCount = 0
+tLoopStart = time.time()
+
+
+with open(args.ipFile, 'rb') as f:
+    csvreader = csv.reader(f, delimiter=',', quotechar='|')
+    
+    with open(args.opFile, 'wb') as g:
+        csvwriter = csv.writer(g, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        
+        while True:
+            for row in csvreader:
+                csvwriter.writerow(row)
+                
+                rowCount +=1
+                if rowCount % SMP_BATCH == 0:
+                    loopCount = loopCount +1
+                    rowCount =0
+                    g.flush()
+                    while time.time() < (tLoopStart + loopCount * batch_period):
+                        time.sleep(0.01)  
+                    print (time.time())
+                
+            if loopCount > 1000000: #if you forget it in the background
+                break
+         
