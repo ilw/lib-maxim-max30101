bde2eec33856b769b6f584692a4e577ba20287d6
ilw <ian_l_williams@hotmail.com>
2020-05-19 10:12:30 +0100

    Working - basic functionality and dumps to a csv file

--- examples/heart_beat/main.py
+++ examples/heart_beat/main.py
@@ -1,94 +1,104 @@
 ################################################################################
 # Heart Rate Example
 #
-# Created: 2017-03-31 10:22:42.543966
+# Created: 19/5/2020
 #
 ################################################################################
+#
+#import streams
+import max30101
+import csv
+from time import sleep
+from datetime import datetime
+from gpiozero import Button  # A button is a good approximation for what we need, a digital active-low trigger
+import numpy as np
+import sys
 
-import streams
-from maxim.max30101 import max30101
+buff_red=[]
+buff_ir=[]
+buff_green=[]
+mode = max30101.MAX30101_MODE_SPO2
 
-streams.serial()
+max = max30101.MAX30101()
+now = datetime.now()
+filename = now.strftime("%d-%m-%Y_%H-%M-%S") + ".csv"
 
-def check_for_beat(obj, smp):
-    beat_detected = False
-    obj._smp_buff[obj._cnt] = smp-obj._prev
-    obj._prev=smp
-    obj._cnt += 1
-    if obj._cnt == 8:
-        obj._cnt = 0
-    smp_min = min(obj._smp_buff)
-    if obj._smp_buff[(obj._cnt-4)&0x7] == smp_min and obj._smp_buff[(obj._cnt-5)&0x7] != 0 and smp_min <= obj._thr:
-        if smp_min >= -2000 and smp_min <= -20:
-            obj._thr = (obj._thr + obj._smp_buff[(obj._cnt-4)&0x7]*.6)/2
-        beat_detected = True
-        obj._smp_buff = [0]*8        
-    return beat_detected
 
-def detect_pulse(obj):   
-    cnt = 0
-    idx = 0
-    hr_avg = 0
-    HR_AVG_SIZE = 10
-    rate = [0]*HR_AVG_SIZE
-    while True:
-        try:
-            val = obj.read_raw_samples(6)
-            hr = (val[3]<<16 | val[4]<<8 | val[5])
-            cnt += 1
-            res = check_for_beat(obj, hr)
-            if res:
-                dt = cnt*obj._dt
-                cnt = 0
-                bpm = 60000/dt
-                if bpm < 255 and bpm > 20:
-                    rate[idx] = bpm
-                    idx += 1
-                    if idx == HR_AVG_SIZE:
-                        idx = 0
-                    hr_avg = 0
-                    for rr in rate:
-                        hr_avg += rr
-                    obj.hr_avg = hr_avg/HR_AVG_SIZE
-            if cnt >= 3000/obj._dt:
-                cnt = 0
-                obj._thr = -20
-                rate = [0]*HR_AVG_SIZE
-                obj.hr_avg = 0
-            obj.clear_fifo()
-            sleep(obj._dt)
-        except Exception as e:
-            print("ErrorT", e)
-            sleep(1000)
 
+def read_data():
+    data_red = []
+    data_ir = []
+    data_green = []
+    
+    interrupts = max.read_triggered_interrupt()
+    #print(interrupts)
+    
+    #when fifo almost_full is triggered there should be at least 17 samples from each active LED 
+    
+    if max.led_mode == max30101.MAX30101_MODE_HR:
+        raw = max.read_raw_samples(17*3)
+        #rawArray = np.asarray(raw)
+        for i in range(len(rawArray)/3):
+            data_red.append(rawArray[i*3]*65536 + rawArray[i*3+1]*256 + rawArray[i*3+2]) #TODO could be written more elegantly...
+            
+    elif max.led_mode == max30101.MAX30101_MODE_SPO2:
+        raw = max.read_raw_samples(17*3*2)
+        rawArray = np.asarray(raw)
+        for i in range(len(rawArray)/6):
+            data_red.append(rawArray[i*6]*65536 + rawArray[i*6+1]*256 + rawArray[i*6+2])
+            data_ir.append(rawArray[i*6+3]*65536 + rawArray[i*6+4]*256 + rawArray[i*6+5])
+            
+    else : 
+        raw = max.read_raw_samples(17*3*3)
+        rawArray = np.asarray(raw)
+        for i in range(len(rawArray)/9):
+            data_red.append(rawArray[i*9]*65536 + rawArray[i*9+1]*256 + rawArray[i*9+2])
+            data_ir.append(rawArray[i*9+3]*65536 + rawArray[i*9+4]*256 + rawArray[i*9+5])
+            data_green.append(rawArray[i*9+6]*65536 + rawArray[i*9+7]*256 + rawArray[i*9+8])
+    
+    
+    #pad the lists to the same length
+    if len(data_ir) < len(data_red):
+        data_ir.extend([0]*len(data_red))
+    if len(data_green) < len(data_red):
+        data_green.extend([0]*len(data_red))    
+    #print(data_red)
+    #print(data_ir)
+    #print(data_green)
+    with open(filename, 'a') as csvfile:
+        datawriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for i in range(len(data_red)):
+            datawriter.writerow([data_red[i] ,data_ir[i] ,data_green[i]])
+    sys.stdout.write( ".")
+    
 # create an instance of the MAX30101 class
 try:
     # Setup sensor
-    # This setup is referred to max30101 mounted on hexiwear device 
-    # Power up and init sequence for max30101 inside hexiwear device
-    pinPowerOn = D70
-    digitalWrite(pinPowerOn,HIGH)
-    pinMode(pinPowerOn,OUTPUT)
-    # init sensor
-    max = max30101.MAX30101(I2C0)
+    # This setup is referred to max30101 on a Heartrate 4 click board 
+    # connected to raspberry pi i2c pins sda - 3 & scl - 5), interrupt on pin 26
+    # 3.3V & 5V from appropriate pins
+    
     print("start...")
-    max.start()
-    max._thr = -20
-    max._dt = 50
-    max._prev = 0
-    max._smp_buff = [0]*8
-    max._cnt = 0
-    max.hr_avg = 0
+    
+    interrupt = Button(26,pull_up = True)  # Setup the interrup pin
+    interrupt.when_activated = read_data  # Connect the interrupt
+    
     print("init...")
     max.init()
     # start a thread that reads and process raw data from the sensor
-    thread(detect_pulse,max)
+    #thread(detect_pulse,max)
     print("Ready!")
     print("---------------------------")
+    max.set_fifo_afv(15) #set fifo almost full value to max to minimise number of reads 
+    max.enable_interrupt(sources=["full"])  # Set up a trigger to fire when the FIFO buffer (on the MAX30100) fills up.
+                                             # You could also use INTERRUPT_HR to get a trigger on every measurement.
+
+
+
 except Exception as e:
     print(e)
 
 while True:
-    print("Heart Rate:", int(max.hr_avg))
-    print("---------------------------")
-    sleep(3000)
+    sleep(1)
+    sys.stdout.flush()
+    
