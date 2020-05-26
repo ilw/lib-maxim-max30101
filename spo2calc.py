518be5ab2ca3b036815567b638ffc27a8bafe0e1
ilw <ian_l_williams@hotmail.com>
2020-05-26 12:00:57 +0100

    Switch to python3 and pyqtgraph to make it easier to embed in a gui.
    Needs the dev branch of pyqtgraph otherwise you get an error about time not having a clock function (python 3.8 removed that function). Best way is to
    if you have pyqtgraph already - uninstall it   with "pip uninstall pyqtgraph"
    then get the latest pyqtgraph:
    git clone git clone https://github.com/pyqtgraph/pyqtgraph.git
    in the repo run "python setup.py install"

--- spo2calc.py
+++ spo2calc.py
@@ -3,9 +3,11 @@ import sys
 import argparse
 import time
 import numpy as np
-import matplotlib.pyplot as plt
 from collections import deque
-from matplotlib.animation import FuncAnimation
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+import os
+
 
 ###########################
 # Read in Arguments & set default parameters
@@ -17,7 +19,7 @@ parser.add_argument("--ipFile", default='ppg.csv',
 args = parser.parse_args()
 
 BUFF_SIZE = 250
-UPDATE_DELAY = 1000
+UPDATE_DELAY = 1
 DISP_LENGTH = 100
 LINE_SIZE = 30  #max line length with safety margin. Its usually 17 for spo2
 
@@ -35,28 +37,43 @@ increment = 2.6e05
 
 xData = np.arange(DISP_LENGTH)
 
+#windowWidth = 500       
+#ptr = -windowWidth                      # set first x position
+ptr = -DISP_LENGTH                      # set first x position
+
+
+### QtApp Initialisation #####
+app = QtGui.QApplication([])            # you MUST do this once (initialize things)
+win = pg.GraphicsWindow(title="PPG signal") # creates a window
+p = win.addPlot(title="Realtime plot")  # creates empty space for the plot in the window
+curve = p.plot()                        # create an empty "plot" (a curve to plot)
+##########################################
+
+
+
+
+#Seek to near the end of the file (in case a large recording has been done)
 readPos = 0
-with open(args.ipFile, 'rb') as f:
-    f.seek(0,2)
+with open(args.ipFile, 'r') as f:
+    f.seek(0,os.SEEK_END)
     fileSize = f.tell()
     if fileSize > BUFF_SIZE * LINE_SIZE:
-        f.seek(-BUFF_SIZE * LINE_SIZE, 2) #go that far from the end
-        pos = f.tell()
+        pos = f.tell() - BUFF_SIZE * LINE_SIZE
+        f.seek(pos, os.SEEK_SET)   # go backwards 3 bytes
         s=f.read(LINE_SIZE)
-        offset = s.find("\r\n")
+        offset = s.find("\r\n") #b'\x0D\x0A')  #\r\n
         readPos = pos+offset+2
 
 
-#read through all the lines and load the last BUFF_SIZE into memory
+#read through the new data lines and load values into memory
 def readPpgData(): 
     global readPos, temp_red, temp_ir, temp_green, args
     
-    with open(args.ipFile, 'rb') as f:
+    with open(args.ipFile, 'r') as f:
         csvreader = csv.reader(f, delimiter=',', quotechar='|')
-        f.seek(readPos,0)
+        f.seek(readPos,os.SEEK_SET)
         for row in csvreader:
             if len(row)==3:
-                readPos = f.tell()
                 
                 #What does this increment do???
                 new_red = float(row[0]) - (np.floor(float(row[0])/increment))*increment
@@ -75,22 +92,11 @@ def readPpgData():
                 temp_ir[-1] = new_ir
                 temp_green[-1] = new_green
             
+        readPos = f.tell() #Accuracy of this depends on how csvreader deals with incomplete lines
 
-
-
-
-fig,ax = plt.subplots()
-ln, = plt.plot([],[])
-
-def initPlt():
-    global ax, ln
-    #pass
-    ax.set_xlim(0,DISP_LENGTH)
-    ax.set_ylim(50,100)
-    return ln,
     
-def updatePlt(i):
-    global spo2_store, temp_red, temp_ir, temp_green, ln
+def updatePlt():
+    global spo2_store, temp_red, temp_ir, temp_green, ptr, curve
     #pass
     readPpgData()
     
@@ -104,16 +110,34 @@ def updatePlt(i):
     spo2_store = np.roll(spo2_store,-1)
     spo2_store[-1] =  spo2
     
-    ln.set_data(xData, spo2_store)
-    return ln,
+    ptr += 1                              # update x position for displaying the curve
+    curve.setData(spo2_store)                     # set the curve with this data
+    curve.setPos(ptr,0)                   # set x position in the graph to 0
+    QtGui.QApplication.processEvents()    # you MUST process the plot now
     
     
 
 
 readPpgData() #do initial read
 
-ani = FuncAnimation(fig, updatePlt, interval = UPDATE_DELAY, init_func=initPlt, blit=True)
-plt.show()
 
 
+
+
+
+
+
+
+### MAIN PROGRAM #####    
+# this is a brutal infinite loop calling your realtime data plot
+while True: 
+    print (time.time(), tLoopStart + UPDATE_DELAY * loopCount)
+    while time.time() < tLoopStart + UPDATE_DELAY * loopCount:
+        time.sleep(0.1*UPDATE_DELAY)
+    updatePlt()
+    loopCount +=1
+
+### END QtApp ####
+pg.QtGui.QApplication.exec_() # you MUST put this at the end
+##################
 
